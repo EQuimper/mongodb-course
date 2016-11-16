836e429f839cd5770e6d488c1ebf520af4277e1a
EQuimper <quimperemanuel@gmail.com>
2016-11-16 13:46:14 -0500

    Put the mongoose.connect in a before function

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('DB Connect Good to go!'))
-  .on('error', err => console.warn('Warning', err));
+// Make mongoose use ES6 Promise
+mongoose.Promise = global.Promise;
+
+// only one time before the test run
+before(done => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', err => console.warn('Warning', err));
+});
 
 // Hook is execute before any other test on the test suite
 beforeEach(done => {
