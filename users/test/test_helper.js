770c7f3f6e58985b8a10ecbae923006a34bdd409
EQuimper <quimperemanuel@gmail.com>
2016-11-16 13:10:50 -0500

    Add a hook function for drop the collections

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -4,3 +4,12 @@ mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', () => console.log('DB Connect Good to go!'))
   .on('error', err => console.warn('Warning', err));
+
+// Hook is execute before any other test on the test suite
+beforeEach(done => {
+  // Delete all the records before each test
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
+  });
+});
