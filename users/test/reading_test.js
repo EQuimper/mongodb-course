06807d2f3541628a433746a677bf4d55fed08ff7
EQuimper <quimperemanuel@gmail.com>
2016-11-17 23:30:00 -0500

    Add sorting and skiping

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,12 +2,22 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let joe;
+  let joe, maria, alex, zach;
+  
   beforeEach(done => {
+    alex = new User({ name: 'Alex' });
     joe = new User({ name: 'Joe' });
-    joe.save()
-      .then(() => done());
+    maria = new User({ name: 'Maria' })
+    zach = new User({ name: 'Zach'});
+
+    Promise.all([
+      alex.save(),
+      joe.save(),
+      maria.save(),
+      zach.save()
+    ]).then(() => done());
   });
+
   it('finds all users with a name of joe', done => {
     User.find({ name: 'Joe' })
       .then(users => {
@@ -22,6 +32,20 @@ describe('Reading users out of the database', () => {
     User.findOne({ _id: joe._id})
       .then(user => {
         assert(user.name === 'Joe');
+        done();
+      });
+  });
+
+  it('can skip and limit the result set', done => {
+    // -Alex- [Joe Maria] Zach
+    User.find({})
+      .sort({ name: 1 }) // make it ascendant
+      .skip(1)
+      .limit(2)
+      .then(users => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Maria');
         done();
       });
   });
