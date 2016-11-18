33194faad9e49af38f8fc843ea567846d7974f40
EQuimper <quimperemanuel@gmail.com>
2016-11-18 02:22:21 -0500

    Done the upstar app

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,49 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist
+    .find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 }) // key interpolation
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([
+    query,
+    Artist.find(buildQuery(criteria)).count()
+  ])
+    .then(values => {
+      return {
+        all: values[0],
+        count: values[1],
+        offset,
+        limit
+      }
+    });
 };
+
+const buildQuery = criteria => {
+  const query = {};
+
+  if (criteria.name) {
+    const { name } = criteria;
+    query.$text = { $search: name }
+  }
+
+  if (criteria.age) {
+    const { age } = criteria;
+    query.age = {
+      $gte: age.min,
+      $lte: age.max
+    }
+  }
+
+  if (criteria.yearsActive) {
+    const { yearsActive } = criteria;
+    query.yearsActive = {
+      $gte: yearsActive.min,
+      $lte: yearsActive.max
+    }
+  }
+
+  return query;
+}
